bb22fab59622cc77dcc1ec3d046cd496bb0557f0
Groshaq <gros.shaq@gmail.com>
2025-11-20 23:15:50 +0100

    Update MDLP - AntiDeco.user.js

--- MDLP - AntiDeco.user.js
+++ MDLP - AntiDeco.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MDLP - anti-déconnexion
 // @namespace    http://tampermonkey.net/
-// @version      1.7
+// @version      1
 // @description  Auto login + keep-alive sur la page Stitch Noël, avec panneau d’état et logs debug.
 // @match        https://www.maisondelapresse.com/mini-figurine-stitch-edition-noel.html
 // @run-at       document-end
